7c78cdee5ec5bbfc261ce5b989f4b80a6fb09961
Sam Tingleff <sam@tingleff.com>
2014-07-10 21:50:40 -0700

    added a soundcloud plugin

--- src/main/java/com/tingleff/yassg/formats/mk/MarkdownTemplateEngine.java
+++ src/main/java/com/tingleff/yassg/formats/mk/MarkdownTemplateEngine.java
@@ -5,11 +5,13 @@ import org.markdown4j.Markdown4jProcessor;
 import com.tingleff.yassg.formats.TemplateEngine;
 import com.tingleff.yassg.formats.TemplateInstance;
 import com.tingleff.yassg.formats.mk.plugin.FlickrPlugin;
+import com.tingleff.yassg.formats.mk.plugin.SoundcloudPlugin;
 
 public class MarkdownTemplateEngine implements TemplateEngine {
 
 	private Markdown4jProcessor p = new Markdown4jProcessor()
-			.registerPlugins(new FlickrPlugin());
+			.registerPlugins(new FlickrPlugin())
+			.registerPlugins(new SoundcloudPlugin());
 
 	@Override
 	public TemplateInstance parse(String template) {
---
+++ src/main/java/com/tingleff/yassg/formats/mk/plugin/SoundcloudPlugin.java
@@ -0,0 +1,25 @@
+package com.tingleff.yassg.formats.mk.plugin;
+
+import java.util.List;
+import java.util.Map;
+
+import org.markdown4j.Plugin;
+
+public class SoundcloudPlugin extends Plugin {
+
+	public SoundcloudPlugin() {
+		super("soundcloud");
+	}
+
+	@Override
+	public void emit(StringBuilder sb, List<String> lines,
+			Map<String, String> params) {
+		for (String line : lines) {
+			String track = line;
+			sb.append(
+					"<iframe width=\"100%\" height=\"166\" scrolling=\"no\" frameborder=\"no\" src=\"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/{track}&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false\"></iframe>"
+					.replace("{track}", track));
+		}
+	}
+
+}
