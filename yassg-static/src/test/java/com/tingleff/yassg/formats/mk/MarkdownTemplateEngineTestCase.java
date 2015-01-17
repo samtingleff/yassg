361b74292dad57caafd82d0c8ba0a670598fcf50
Sam Tingleff <sam@tingleff.com>
2015-01-16 16:20:38 -0800

    added test case on soundcloud plugin

--- yassg-static/src/test/java/com/tingleff/yassg/formats/mk/MarkdownTemplateEngineTestCase.java
+++ yassg-static/src/test/java/com/tingleff/yassg/formats/mk/MarkdownTemplateEngineTestCase.java
@@ -34,4 +34,14 @@ public class MarkdownTemplateEngineTestCase {
 		Assert.assertNotNull(result);
 		Assert.assertEquals("<iframe src=\"https://www.flickr.com/photos/samtingleff/1934134452454/player/\" width=\"450\" height=\"375\" frameborder=\"0\" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>", result);
 	}
+
+	@Test
+	public void soundcloudPlugin() throws IOException {
+		String t = "%%% soundcloud height=150\n1934134452454\n%%%";
+		TemplateInstance ti = te.parse(t);
+		Assert.assertNotNull(ti);
+		String result = ti.render();
+		Assert.assertNotNull(result);
+		Assert.assertEquals("<iframe width=\"100%\" height=\"150\" scrolling=\"no\" frameborder=\"no\" src=\"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1934134452454&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false\"></iframe>", result);
+	}
 }
