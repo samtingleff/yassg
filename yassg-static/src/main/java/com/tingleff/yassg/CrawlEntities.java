1c90920c653a3b7648f7da29737887555c166950
Sam Tingleff <sam@tingleff.com>
2015-11-27 22:56:07 -0800

    iterate over crawled links to build feature vectors

---
+++ yassg-static/src/main/java/com/tingleff/yassg/CrawlEntities.java
@@ -0,0 +1,80 @@
+package com.tingleff.yassg;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.tingleff.yassg.semantic.NamedEntity;
+import com.tingleff.yassg.semantic.NamedEntityResponse;
+import com.tingleff.yassg.semantic.SemanticDB;
+import com.tingleff.yassg.semantic.SemanticDBImpl;
+
+public class CrawlEntities {
+
+	public static void main(String[] args) throws Exception {
+		CrawlEntities m = new CrawlEntities();
+		new JCommander(m).parse(args);
+		m.run();
+	}
+
+	@Parameter(names = "-alchemyCache", required = false)
+	private String alchemyCacheDir;
+
+	private SemanticDB db;
+
+	public void run() throws Exception {
+		db = new SemanticDBImpl(alchemyCacheDir);
+
+		// build a map of features to IDs
+		Map<String, Integer> featureMap = buildFeatureMap(db.iterator());
+
+		// build a map of ids to feature vector
+		Map<String, double[]> vectors = buildVectors(db.iterator(), featureMap);
+	}
+
+	public Map<String, double[]> buildVectors(Iterator<NamedEntityResponse> iter, Map<String, Integer> featureMap) {
+		Map<String, double[]> result = new HashMap<String, double[]>();
+		while (iter.hasNext()) {
+			NamedEntityResponse ner = iter.next();
+			double[] features = featureVector(ner, featureMap);
+			result.put(ner.getUrl(), features);
+		}
+		return result;
+	}
+
+	public double[] featureVector(NamedEntityResponse ner, Map<String, Integer> featureMap) {
+		double[] result = new double[featureMap.size()];
+		Iterator<NamedEntity> entities = ner.iterator();
+		while (entities.hasNext()) {
+			NamedEntity ne = entities.next();
+			Integer index = featureMap.get(featureKey(ne));
+			result[index.intValue()] = ne.getScore();
+		}
+		return result;
+	}
+
+	private Map<String, Integer> buildFeatureMap(Iterator<NamedEntityResponse> iter) {
+		int id = 0;
+		Map<String, Integer> m = new HashMap<String, Integer>();
+		while (iter.hasNext()) {
+			NamedEntityResponse ner = iter.next();
+			Iterator<NamedEntity> entities = ner.iterator();
+			while (entities.hasNext()) {
+				NamedEntity ne = entities.next();
+				String key = featureKey(ne);
+				Integer i = m.get(key);
+				if (i == null) {
+					m.put(key, new Integer(id));
+					++id;
+				}
+			}
+		}
+		return m;
+	}
+
+	private String featureKey(NamedEntity ne) {
+		return String.format("%1$s:%2$s", ne.getType(), ne.getText());
+	}
+}
